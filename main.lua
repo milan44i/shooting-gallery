cd656ad74286e70c7ac9d249c852212b924de97a
milan44i <mln6stankovic@gmail.com>
2025-05-21 15:51:57 +0200

    end screen, cooldown timer

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 function love.load()
-  target = {radius = 50}
+  target = {x = 0, y = 0, radius = 50}
   score = 0
   timer = 10
-  gameState = 1
+  gameState = 'start'
+  cooldownTimer = 0
 
   gameFont = love.graphics.newFont(40)
 
@@ -15,13 +16,18 @@ function love.load()
 end
 
 function love.update(dt)
-  if timer > 0 then
+  if gameState == 'play' and timer > 0 then
     timer = timer - dt
   end
 
   if timer < 0 then
     timer = 0
-    gameState = 1
+    gameState = 'end'
+    cooldownTimer = 1 
+  end
+  
+  if cooldownTimer > 0 then
+    cooldownTimer = cooldownTimer - dt
   end
 end
 
@@ -32,17 +38,21 @@ function love.draw()
   love.graphics.setFont(gameFont)
   love.graphics.print("Score: " .. score, 10, 10)
 
-  if gameState == 1 then
+  if gameState == 'start' then
     love.graphics.printf("Click to Start", 0, 200, love.graphics.getWidth(), "center")
-  elseif gameState == 2 then
+  elseif gameState == 'play' then
     love.graphics.printf("Time: " .. math.ceil(timer), 0, 10, love.graphics.getWidth(), "center")
     love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  elseif gameState == 'end' then
+    love.graphics.printf("Game Over", 0, 100, love.graphics.getWidth(), "center")
+    love.graphics.printf("Final Score: " .. score, 0, 200, love.graphics.getWidth(), "center")
+    love.graphics.printf("Click to go to Main Menu", 0, 300, love.graphics.getWidth(), "center")
   end
   love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-  if button == 1 and gameState == 2 then
+  if button == 1 and gameState == 'play' then
     local distance = distance(x, y, target.x, target.y)
     if distance <= target.radius then
       score = score + 1
@@ -53,11 +63,13 @@ function love.mousepressed(x, y, button, istouch, presses)
         score = 0
       end
     end
-  elseif button == 1 and gameState == 1 then
-    gameState = 2
-    timer = 10
-    score = 0
+  elseif button == 1 and gameState == 'start' then
+    gameState = 'play'
+    resetState()
     generateTargetPosition()
+  elseif button == 1 and gameState == 'end' and cooldownTimer <= 0 then
+    gameState = 'start'
+    resetState()
   end
 end
 
@@ -68,4 +80,9 @@ end
 function generateTargetPosition()
   target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
   target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+end
+
+function resetState()
+  score = 0
+  timer = 10
 end
